207653d28ebc06d853ee39e181be8bd36e4b434a
Sun Dong <sundong@exidea.tech>
2022-11-04 09:38:46 +0800

    updated launchpad contract

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -7,6 +7,7 @@ import "./Interface8/IERC20Metadata.sol";
 import "./Interface8/IPancakeRouter02.sol";
 import "./Lib8/Ownable.sol";
 import "./Lib8/ReentrancyGuard.sol";
+import "./Lib8/EnumerableSet.sol";
 
 interface IPancakeFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
@@ -70,25 +71,45 @@ struct PresaleData {
     TeamVesting teamVestingData;
 }
 
-contract Presale is Ownable, ReentrancyGuard {
+struct Contributes {
+    address contributor;
+    uint amount;
+}
 
+contract Presale is Ownable, ReentrancyGuard {
+    // contributors
     mapping(address => uint) public contributes;
-    uint public collected = 0;
-    uint tokenDecimals;
-    address public pcsRouter = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
-    mapping(address => bool) public whitelisted;
+    EnumerableSet.AddressSet private _contributors;
+
+    // whitelisted users
+    EnumerableSet.AddressSet private _whitelistedUsers;
+
+    // user claims
     mapping(address => uint) private userClaims;
 
-    uint claimedTeamVesting = 0;
-    uint finishedTime = 0;
-    bool finished = false;
-    
+    // collected BNB amount
+    uint public collected = 0;
+
+    // presale info
     PresaleData presaleData;
+    uint tokenDecimals;
+    address public pcsRouter = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    
+    // claimed amount of team
+    uint claimedTeamVesting = 0;
+
+    // presale finish info
+    uint public finishedTime = 0;
+    bool public finished = false;
+
+    // presale cancel info
+    uint public cancelTime = 0;
+    bool public canceled = false;
+    
 
     constructor(PresaleData memory _presale, address _router) {
         presaleData = _presale;
-        pcsRouter = _router;
-        
+        pcsRouter = _router;        
         tokenDecimals = IERC20Metadata(presaleData.token).decimals();
     }
 
@@ -97,14 +118,14 @@ contract Presale is Ownable, ReentrancyGuard {
         _;
     }
     
-    receive() external payable {
-        _contribute(msg.sender, msg.value);
-    }
-
     function setMetaData(string memory logo_link, string memory description, string memory others) external onlyCreator {
         presaleData.logo_link = logo_link;
         presaleData.description = description;
         presaleData.metadata = others;
+    }
+
+    receive() external payable {
+        _contribute(msg.sender, msg.value);
     }
 
     function contribute() payable external {
@@ -117,9 +138,13 @@ contract Presale is Ownable, ReentrancyGuard {
         require (block.timestamp <= presaleData.end_time, "Presale already ended");
 
         if (presaleData.whitelist) {
-            require (whitelisted[msg.sender], "You're not whitelisted");
+            require (isWhitelistedUser(msg.sender), "You're not whitelisted");
         }
 
+        if (!isContributor(user)) {
+            _addContributor(user);
+        }
+        
         uint left = presaleData.hardcap - collected;
 
         uint contributeAmount = amount;
@@ -136,13 +161,12 @@ contract Presale is Ownable, ReentrancyGuard {
             contributeAmount = available;
         }
 
-        collected += contributeAmount;
-
         if (returnAmount > 0) {
             payable(user).transfer(returnAmount);
         }
-
-        contributes[user] = contributeAmount;
+        
+        contributes[user] += contributeAmount;
+        collected += contributeAmount;
     }
 
     function claim() external nonReentrant {
@@ -180,6 +204,15 @@ contract Presale is Ownable, ReentrancyGuard {
 
         payable(msg.sender).transfer(contributes[msg.sender]);
         contributes[msg.sender] = 0;
+        _delContributor(msg.sender);
+    }
+
+    function emergencyWithdraw() external nonReentrant {
+        require (contributes[msg.sender] > 0, "You have not contributed");
+
+        payable(msg.sender).transfer(contributes[msg.sender]);
+        contributes[msg.sender] = 0;
+        _delContributor(msg.sender);
     }
 
     function finalize() external onlyCreator {
@@ -219,14 +252,39 @@ contract Presale is Ownable, ReentrancyGuard {
         return 1;
     }
     
+    function getClaimAmount() view external returns(uint) {
+        if(!finished || contributes[msg.sender] < 1 || collected < presaleData.hardcap) return 0;
+        uint amount = contributes[msg.sender] * presaleData.presale_rate / (10 ** (18 - tokenDecimals));
+
+        if (amount <= userClaims[msg.sender]) return 0;
+
+        if (presaleData.presaleVesting) {
+            uint claimable = amount * presaleData.presaleVestingData.firstRelease / 100 + amount * (block.timestamp - finishedTime) / presaleData.presaleVestingData.cycle * presaleData.presaleVestingData.cycleRelease / 100;
+
+            if (claimable > amount) {
+                claimable = amount;
+            }
+
+            if (claimable <= userClaims[msg.sender]) return 0;
+
+            return claimable - userClaims[msg.sender];
+        } else {
+            return amount;
+        }        
+    }
+    
     function getPresaleData() view public returns(PresaleData memory) {
         return presaleData;
     }
 
-    function whitelistUsers(address[] calldata users, bool _whitelisted) external onlyCreator {
+    function addWhitelistUsers(address[] calldata users, bool _whitelisted) external onlyCreator {
         uint i;
         for (i = 0; i < users.length; i+=1) {
-            whitelisted[users[i]] = _whitelisted;
+            if (_whitelisted){
+                _addWhitelistUser(users[i]);
+            } else {
+                _delWhitelistUser(users[i]);
+            }            
         }
     }
 
@@ -253,5 +311,78 @@ contract Presale is Ownable, ReentrancyGuard {
         claimedTeamVesting += claimableAmount;
 
         IERC20(presaleData.token).transfer(payable(to), claimableAmount);
+    }
+
+    function cancel() external onlyCreator {
+        require (canceled == false, "Presale failed or not ended yet");
+
+        canceled = true;
+        cancelTime = block.timestamp;
+    }
+
+
+    // contributors functions
+    function _addContributor(address _addUser) internal returns (bool) {
+        require(_addUser != address(0), "_addUser is the zero address");
+        return EnumerableSet.add(_contributors, _addUser);
+    }
+
+    function _delContributor(address _delUser) internal returns (bool) {
+        require(_delUser != address(0), "_delUser is the zero address");
+        return EnumerableSet.remove(_contributors, _delUser);
+    }
+
+    function getContributorLength() public view returns (uint256) {
+        return EnumerableSet.length(_contributors);
+    }
+
+    function getContributor(uint256 _index) public view onlyCreator returns (address){
+        require(_index <= getContributorLength() - 1, "index out of bounds");
+        return EnumerableSet.at(_contributors, _index);
+    }
+
+    function getContributorArr() public view onlyCreator returns (Contributes[] memory) {
+        Contributes[] memory contributeArr = new Contributes[](getContributorLength());
+        for (uint i = 0; i < getContributorLength(); i+=1) {
+            contributeArr[i] = 
+                Contributes({
+                    contributor: getContributor(i),
+                    amount: contributes[getContributor(i)]
+                });
+        }
+        return contributeArr;
+    }
+
+    function isContributor(address account) public view returns (bool) {
+        return EnumerableSet.contains(_contributors, account);
+    }
+
+
+    // whitelisted users functions
+    function _addWhitelistUser(address _addUser) internal returns (bool) {
+        require(_addUser != address(0), "_addUser is the zero address");
+        return EnumerableSet.add(_whitelistedUsers, _addUser);
+    }
+
+    function _delWhitelistUser(address _delUser) internal returns (bool) {
+        require(_delUser != address(0), "_delUser is the zero address");
+        return EnumerableSet.remove(_whitelistedUsers, _delUser);
+    }
+
+    function getWhitelistedUsersLength() public view returns (uint256) {
+        return EnumerableSet.length(_whitelistedUsers);
+    }
+
+    function getWhitelistedUser(uint256 _index) public view onlyCreator returns (address){
+        require(_index <= getWhitelistedUsersLength() - 1, "index out of bounds");
+        return EnumerableSet.at(_whitelistedUsers, _index);
+    }
+
+    function getWhitelistedUsersArr() public view returns (bytes32[] memory) {
+        return _whitelistedUsers._inner._values;
+    }
+
+    function isWhitelistedUser(address account) public view returns (bool) {
+        return EnumerableSet.contains(_whitelistedUsers, account);
     }
 }
